f57feba5911e4bfcb718cf564eb71ad84845a9b8
Ryan <yzr1914001753@gmail.com>
2024-09-11 10:01:05 -0400

    shapetype

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
@@ -12,9 +12,10 @@ public class Renderer {
     void draw() {
         ShapeType type=shape.getShape();
         double area = shape.getArea();
-
+        
         // assume implementation
         
         System.out.println("Shape printed\n" + "Its area is " + area);
+        System.out.println(" Its type is " + type);
     }
 }
